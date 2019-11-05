fe999ae076cfb84ed07c7bbf4767f25e8043d4ef
m <you@example.com>
2019-11-04 21:26:20 -0500

    Add build prod

--- build/precache-manifest.84f56a0db89b74f1eb1b5c90e7b9d45e.js
+++ build/precache-manifest.84f56a0db89b74f1eb1b5c90e7b9d45e.js
@@ -5,34 +5,34 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "edbae7e3aa6aedf8636b",
-    "url": "/static/css/main.f2648d9a.chunk.css"
+    "url": "https://sumelio.github.io/yoxi_game/build/static/css/main.f2648d9a.chunk.css"
   },
   {
     "revision": "4613dfdc6e62626704a1",
-    "url": "/static/js/2.a6635532.chunk.js"
+    "url": "https://sumelio.github.io/yoxi_game/build/static/js/2.a6635532.chunk.js"
   },
   {
     "revision": "edbae7e3aa6aedf8636b",
-    "url": "/static/js/main.aa61cd9e.chunk.js"
+    "url": "https://sumelio.github.io/yoxi_game/build/static/js/main.aa61cd9e.chunk.js"
   },
   {
     "revision": "408d055a25c1066b26e3",
-    "url": "/static/js/runtime-main.589f3f6c.js"
+    "url": "https://sumelio.github.io/yoxi_game/build/static/js/runtime-main.589f3f6c.js"
   },
   {
     "revision": "37e34e13206f79a1018da0b9bb0139df",
-    "url": "/static/media/arrow-next.37e34e13.png"
+    "url": "https://sumelio.github.io/yoxi_game/build/static/media/arrow-next.37e34e13.png"
   },
   {
     "revision": "c75ac40a1066ab0becd2779fe415cea4",
-    "url": "/static/media/background.c75ac40a.jpg"
+    "url": "https://sumelio.github.io/yoxi_game/build/static/media/background.c75ac40a.jpg"
   },
   {
     "revision": "0be82ec1eca578aa4dc1189676913e46",
-    "url": "/static/media/intro.0be82ec1.mp3"
+    "url": "https://sumelio.github.io/yoxi_game/build/static/media/intro.0be82ec1.mp3"
   },
   {
     "revision": "71efb88d629498443313600823229a20",
-    "url": "/static/media/menu.71efb88d.mp3"
+    "url": "https://sumelio.github.io/yoxi_game/build/static/media/menu.71efb88d.mp3"
   }
 ]);
