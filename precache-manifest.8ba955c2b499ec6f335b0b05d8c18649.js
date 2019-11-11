49abdb0d4129044cbb21ea74e296bf69546b667c
m <you@example.com>
2019-11-10 21:18:57 -0500

    Add index.html and build

---
+++ precache-manifest.8ba955c2b499ec6f335b0b05d8c18649.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c699fe2fdb8e58894dda44bdf291d042",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3393d1123f30f617a05",
+    "url": "/static/css/main.32014638.chunk.css"
+  },
+  {
+    "revision": "c62874045d7755a3f8b6",
+    "url": "/static/js/2.9ebd7224.chunk.js"
+  },
+  {
+    "revision": "d3393d1123f30f617a05",
+    "url": "/static/js/main.36ae20ef.chunk.js"
+  },
+  {
+    "revision": "408d055a25c1066b26e3",
+    "url": "/static/js/runtime-main.589f3f6c.js"
+  },
+  {
+    "revision": "103e1d457246ff60247ec0639c10c0b5",
+    "url": "/static/media/a.103e1d45.png"
+  },
+  {
+    "revision": "9040dabff1f6f1d7902b8b11e74674af",
+    "url": "/static/media/arrow-back.9040dabf.png"
+  },
+  {
+    "revision": "39c9f110f16c55954fbbbb28a390885b",
+    "url": "/static/media/arrow-next.39c9f110.png"
+  },
+  {
+    "revision": "f91f4d21e7726c7f747c58722955b0b4",
+    "url": "/static/media/background.f91f4d21.jpg"
+  },
+  {
+    "revision": "3a891e46cfc103e0d7c6566e1144e56e",
+    "url": "/static/media/bear.3a891e46.mp4"
+  },
+  {
+    "revision": "cba93b7537b24878a00a4d1264bd6c88",
+    "url": "/static/media/bear.cba93b75.png"
+  },
+  {
+    "revision": "50688ea7eabdfa44280e99f1b89f5aef",
+    "url": "/static/media/e.50688ea7.png"
+  },
+  {
+    "revision": "949aaec56c92f1226e02999d6e471afb",
+    "url": "/static/media/elephan.949aaec5.mp3"
+  },
+  {
+    "revision": "e89e9d1e084ef3bba37aa330c238c357",
+    "url": "/static/media/elephan.e89e9d1e.png"
+  },
+  {
+    "revision": "ee42fe849dd2dc2ca924fc4754170e91",
+    "url": "/static/media/enter.ee42fe84.mp3"
+  },
+  {
+    "revision": "a1dc213d83311033dc880d74786a1c57",
+    "url": "/static/media/i.a1dc213d.png"
+  },
+  {
+    "revision": "0be82ec1eca578aa4dc1189676913e46",
+    "url": "/static/media/intro.0be82ec1.mp3"
+  },
+  {
+    "revision": "471f89b53a632f9effb1e995bd185441",
+    "url": "/static/media/magnet.471f89b5.png"
+  },
+  {
+    "revision": "c9973f753ab4410750dfeae7fdf757fd",
+    "url": "/static/media/magnet.c9973f75.mp3"
+  },
+  {
+    "revision": "71efb88d629498443313600823229a20",
+    "url": "/static/media/menu.71efb88d.mp3"
+  },
+  {
+    "revision": "a291a83a2394cb6dbe6515221493d364",
+    "url": "/static/media/next.a291a83a.mp3"
+  },
+  {
+    "revision": "aa576553d437f1e3ac02794e9ffa039a",
+    "url": "/static/media/o.aa576553.png"
+  },
+  {
+    "revision": "0a43ccf84a141e67cb73f17763483690",
+    "url": "/static/media/rainbow.0a43ccf8.png"
+  },
+  {
+    "revision": "eb58b727a1a93b740372df66b1433893",
+    "url": "/static/media/rainbow.eb58b727.mp3"
+  },
+  {
+    "revision": "d494cdd2d3f43f1a4e4177a9ad7883c7",
+    "url": "/static/media/vowel-first.d494cdd2.mp3"
+  },
+  {
+    "revision": "e456301c2a99ca6269c7fd761637ad9c",
+    "url": "/static/media/yoxi.e456301c.png"
+  }
+]);
